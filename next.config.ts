0c961e24720acc177a89d0a2745ab6f3ce5ba86f
Kaili Kameoka <kaili.kameoka@gmail.com>
2026-05-14 11:44:08 -1000

    Deployment Setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Tells Next.js to produce static HTML/CSS/JS
+  basePath: "/blog-preview-card", // This adds the prefix to all links and assets,
+  trailingSlash: true,
+  images: {
+    unoptimized: true, // Required because GitHub Pages doesn't support Next.js default Image Optimization
+  },
+  // If your site is NOT at a custom domain (e.g., username.github.io/repo-name/)
+  // you MUST set the basePath to your repository name.
+  // basePath: '/your-repo-name',
 };
 
 export default nextConfig;
